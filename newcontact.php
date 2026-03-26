2cc42c2baac920e6927e3b84fa29c63642207f27
weskams <wesleymacharia72@gmail.com>
2026-03-26 23:56:47 +0300

    Initial commit

---
+++ newcontact.php
@@ -0,0 +1,33 @@
+<?php
+// Get form data
+$name = $_POST['name'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$address = $_POST['address'];
+$date_of_birth = $_POST['date_of_birth'];
+$phone = $_POST['phone'];
+$department = $_POST['department'];
+$position = $_POST['position'];
+
+// Create an array
+$contact = [
+    "name" => $name,
+    "gender" => $gender,
+    "email" => $email,
+    "address" => $address,
+    "date_of_birth" => $date_of_birth,
+    "phone" => $phone,
+    "department" => $department,
+    "position" => $position
+];
+
+// Convert to JSON
+$json = json_encode($contact);
+
+// Save to file
+file_put_contents("contacts.txt", $json . PHP_EOL, FILE_APPEND);
+
+// Redirect back or to view page
+header("Location: viewallcontacts.php");
+exit();
+?>
